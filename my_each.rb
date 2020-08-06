0d42a02adb6868c56cda6e11fdea1b3d83a0bf1e
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-06 03:54:57 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  index = 0
+
+  while index < array.size
+    yield array[index]
+
+    index += 1
+
+  end
+  array
 end
